774491b589d80452c07b67184488b48dc5d973ac
Aris Bezas <igoumeninja@gmail.com>
2015-04-21 11:33:47 +0300

    Update AldoDynamic Score

--- bin/data/scripts/my-lua-scripts/iclc/150421.lua
+++ bin/data/scripts/my-lua-scripts/iclc/150421.lua
@@ -1,6 +1,7 @@
 function setup()
    of.setWindowTitle("startUp")
    of.background(0)
+   of.smooth()
 end
 ----------------------------------------------------
 function update()
@@ -9,14 +10,13 @@ end
 ----------------------------------------------------
 function draw()
    of.fill()
-   of.setColor(0,0,0,20)
+   of.setColor(0,0,0,10)
    of.rect(0,0,of.getWidth(), of.getHeight())
 
-   of.setColor(250,30,230,230)
-   of.rect(100,100,100,100)
-   of.setColor(250,30,20,230)   
-   for i=1,10 do
-         of.rect((i*of.getFrameNum()%of.getWidth()),50*i,100,my.myFunction(50))
+   of.setColor(250,30,20,180)   
+   for i=1,400 do
+         of.ellipse((0.05*i*of.getFrameNum()%of.getWidth()),2*i,10,my.myFunction(5))
+         --of.rect((0.05*i*of.getFrameNum()%of.getWidth()),2*i,10,my.myFunction(5))
    end
 end
 ----------------------------------------------------
